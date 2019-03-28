8cd0528fef0e1f646242b80a897b40425bdc53e3
matjac <majacob1@umbc.edu>
2019-03-27 21:24:49 -0400

    Add Functiontalites for Password And Cipher

---
+++ Escape to Planet Earth/Assets/Scripts/CheckCipher.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckCipher : MonoBehaviour
+{
+    private string currentLetters = "";
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int n = 0; n < gameObject.transform.childCount; n++)
+        {
+            currentLetters += gameObject.transform.GetChild(n).GetComponent<InputField>().text;
+        }
+        
+        if (currentLetters == "Cipher")
+        {
+            Debug.Log("Correct cipher!");
+        }
+        
+        currentLetters = "";
+    }
+}
---
+++ Escape to Planet Earth/Assets/Scripts/CheckPassword.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckPassword : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("return"))
+        {
+            if (GetComponent<InputField>().text == "password")
+            {
+                Debug.Log("Correct password!");
+            }
+            
+            else
+            {
+                Debug.Log("Wrong password! Try again!");
+                GetComponent<InputField>().text = "";
+            }
+        }
+    }
+}
