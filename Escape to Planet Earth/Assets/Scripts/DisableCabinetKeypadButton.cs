eba8560a57a4a447056b9f53bb11d0085f99017d
matjac <majacob1@umbc.edu>
2019-05-08 21:05:59 -0400

    Added Code for Cabinet Keypad Button

---
+++ Escape to Planet Earth/Assets/Scripts/DisableCabinetKeypadButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisableCabinetKeypadButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ClickControlLv2.totalDigits == 3 && ClickControlLv2.playerCode == ClickControlLv2.correctCode)
+        {
+            Button AccessButton = GetComponent<Button>();
+            AccessButton.interactable = false;
+        }
+    }
+}
