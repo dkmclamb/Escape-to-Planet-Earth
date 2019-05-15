22825802c1beecb216e40d9372dd0d9ec40c1117
matjac <majacob1@umbc.edu>
2019-05-15 17:28:16 -0400

    Added Code to Disable Level 1 Buttons

---
+++ Escape to Planet Earth/Assets/Scripts/DisableLevel1Buttons.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisableLevel1Buttons : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ClickControl.totalDigits == 3 && ClickControl.playerCode == ClickControl.correctCode)
+        {
+            Button AccessButton = GetComponent<Button>();
+            AccessButton.interactable = false;
+        }
+    }
+}
