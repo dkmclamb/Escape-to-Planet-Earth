dee808269f226b793ec114e45dbd07ccb5dcd8a4
dkmclamb <andrew56@umbc.edu>
2019-05-17 16:19:44 -0400

    Made the final changes and moved things into the right spots in order to get the fullscreen 1920 by 1080 build looking right. Final, albeit demo version of the game.

--- Escape to Planet Earth/Assets/Scripts/ResetGame.cs
+++ Escape to Planet Earth/Assets/Scripts/ResetGame.cs
@@ -20,6 +20,9 @@ public class ResetGame : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
     }
 }
---
+++ Escape to Planet Earth/Assets/Scripts/SkipKeys.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SkipKeys : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad0))
+        {
+            SceneManager.LoadScene("Main Menu");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            SceneManager.LoadScene("Prison Cell");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            SceneManager.LoadScene("Guard Station");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Keypad3))
+        {
+            SceneManager.LoadScene("Docking Bay");
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            SceneManager.LoadScene("Lights Out");
+        }
+    }
+}
